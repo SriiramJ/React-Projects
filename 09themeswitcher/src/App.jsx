994b722b8d688b7774da115664e20cec7e68b916
Sriram <sriram2005j@gmail.com>
2025-02-15 17:09:57 +0530

    minior change done

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,16 +1,13 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ThemeBtn from './components/ThemeBtn';
-import Card from './components/Card.jsx'
-import { ThemeProvider } from './contexts/theme';
-
-
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card.jsx";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
- const [themeMode, setThemeMode] = useState("light");
-
+  const [themeMode, setThemeMode] = useState("light");
 
   const darkTheme = () => setThemeMode("dark");
   const lightTheme = () => setThemeMode("light");
@@ -18,8 +15,7 @@ function App() {
   useEffect(() => {
     document.querySelector("html").classList.remove("dark", "light");
     document.querySelector("html").classList.add(themeMode);
-  },[themeMode]);
-
+  }, [themeMode]);
 
   return (
     <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
@@ -37,4 +33,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
